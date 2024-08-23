7aacd7a499817b7130105a97bd6fce6392af22b4
Seedling <david.r.graham32@gmail.com>
2024-08-23 11:21:22 -0400

    Create Bicycle class, declare properties and functions

--- asgn01/bike-challenge/challenge_01.php
+++ asgn01/bike-challenge/challenge_01.php
@@ -1,1 +1,16 @@
 <?php
+
+class Bicycle
+{
+  var $brand;
+  var $model;
+  var $year;
+  var $description = 'Used bicycle';
+  var $weight_kg = 0.0;
+
+  function name() {}
+
+  function weight_lbs() {}
+
+  function set_weight_lbs() {}
+}
