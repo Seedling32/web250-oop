bbece883119f0739fdd2d42a46af78515e99a424
Seedling <david.r.graham32@gmail.com>
2024-08-23 14:02:45 -0400

    Fill out name() and weight_lbs() functions.

--- asgn01/bike-challenge/challenge_01.php
+++ asgn01/bike-challenge/challenge_01.php
@@ -8,9 +8,15 @@ class Bicycle
   var $description = 'Used bicycle';
   var $weight_kg = 0.0;
 
-  function name() {}
+  function name()
+  {
+    return $this->brand . " " . $this->model . " (" . $this->year . ")";
+  }
 
-  function weight_lbs() {}
+  function weight_lbs()
+  {
+    return floatval($this->weight_kg) * 2.2046226218;
+  }
 
   function set_weight_lbs() {}
 }
