83c9721df8fe4f8e94e1d3a0405b35bbea585e40
Seedling <david.r.graham32@gmail.com>
2024-08-23 15:03:07 -0400

    Add song() and canFly() functions Add them to displayDetails().

--- asgn01/bird-challenge/index.php
+++ asgn01/bird-challenge/index.php
@@ -6,19 +6,33 @@ class Bird
   var $food;
   var $nestPlacement;
   var $conservationLevel;
+  var $song = 'none';
+  var $fly = false;
+
+  function song()
+  {
+    echo 'This birds song goes something like this: ' . $this->song . '<br>';
+  }
+
+  function canFly()
+  {
+    if ($this->fly) {
+      echo 'This bird can fly!';
+    } else {
+      echo 'This bird cannot fly.';
+    }
+  }
 
   function displayDetails()
   {
-    echo 'Bird details<br>';
-    echo 'name: ' . $this->commonName . '<br>';
-    echo 'food: ' . $this->food . '<br>';
-    echo 'nest placement: ' . $this->nestPlacement . '<br>';
-    echo 'conservation level: ' . $this->conservationLevel . '<br>';
+    echo '<h1>Bird details</h1>';
+    echo 'Name: ' . $this->commonName . '<br>';
+    echo 'Food: ' . $this->food . '<br>';
+    echo 'Nest placement: ' . $this->nestPlacement . '<br>';
+    echo 'Conservation level: ' . $this->conservationLevel . '<br>';
+    echo $this->song();
+    echo $this->canFly();
   }
-
-  function songs() {}
-
-  function canFly() {}
 }
 
 $bird1 = new Bird;
@@ -26,9 +40,17 @@ $bird1->commonName = 'Cardinal';
 $bird1->food = 'Worms';
 $bird1->nestPlacement = 'Lower branches';
 $bird1->conservationLevel = 'Not protected';
+$bird1->song = 'drink-your-tea!';
+$bird1->fly = true;
+
+echo $bird1->displayDetails();
 
 $bird2 = new Bird;
-$bird1->commonName = 'Red Tail hawk';
-$bird1->food = 'Rodents';
-$bird1->nestPlacement = 'Tree tops';
-$bird1->conservationLevel = 'Protected';
+$bird2->commonName = 'Red Tail hawk';
+$bird2->food = 'Rodents';
+$bird2->nestPlacement = 'Tree tops';
+$bird2->conservationLevel = 'Protected';
+$bird2->song = 'whatwhat!';
+
+
+echo $bird2->displayDetails();
