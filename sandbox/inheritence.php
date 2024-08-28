c88bddaccaf9b1a0ff393cdc7a5e4fce7d5ae987
Seedling <david.r.graham32@gmail.com>
2024-08-28 13:37:02 -0400

    Create Truck subclass.

--- sandbox/inheritence.php
+++ sandbox/inheritence.php
@@ -11,3 +11,13 @@ class Vehicle
     return "This vehicle is made by {$this->make} and is the {$this->model} model made in ({$this->year}).";
   }
 }
+
+class Truck extends Vehicle
+{
+  public $classification;
+
+  public function display()
+  {
+    return parent::display() . "This vehicle is a {$this->classification}.";
+  }
+}
