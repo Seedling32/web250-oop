f80a321ccfe7a7c29750fa1d4aed96b54b31da79
Seedling <david.r.graham32@gmail.com>
2024-09-10 11:58:19 -0400

    Change eggNum value for flycatcher

--- asgn03-static/bird/Bird.php
+++ asgn03-static/bird/Bird.php
@@ -32,6 +32,7 @@ class YellowBelliedFlyCatcher extends Bird
   public $name = "yellow-bellied flycatcher";
   public $diet = "mostly insects.";
   public $song = "flat chilk";
+  public static $eggNum = "3-4, sometimes 5.";
 }
 
 class Kiwi extends Bird
