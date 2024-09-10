c2c549fc1ecaa7b8e10c0eb55e9422fbee87c81f
Seedling <david.r.graham32@gmail.com>
2024-09-10 12:09:56 -0400

    Finish Bird challenge

--- asgn03-static/bird/index.php
+++ asgn03-static/bird/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>asgn02 Inheritance</title>
+  <title>asgn 02 Inheritance</title>
 </head>
 
 <body>
