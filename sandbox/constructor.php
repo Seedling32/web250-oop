82d16abb6513d9a21970917d2dd7bba73f341c8e
Seedling <david.r.graham32@gmail.com>
2024-09-11 13:16:32 -0400

    Create a practice constructors file. Explore __construct()

---
+++ sandbox/constructor.php
@@ -0,0 +1,38 @@
+<?php
+
+class Sofa
+{
+  public static $instanceCount = 0;
+  public function __construct()
+  {
+    self::$instanceCount++;
+  }
+}
+
+class Couch extends Sofa
+{
+  public static $couchCount = 0;
+  public function __construct()
+  {
+    self::$couchCount++;
+  }
+}
+
+class Loveseat extends Sofa
+{
+  public static $loveCount = 0;
+  public function __construct()
+  {
+    self::$loveCount++;
+  }
+}
+
+$sofa = new Sofa;
+$sofa2 = new Sofa;
+$couch = new Couch;
+$loveSeat = new Loveseat;
+
+
+echo 'instance count total: ' . Sofa::$instanceCount . '<br>';
+echo 'instance count of couch: ' . Couch::$couchCount . '<br>';
+echo 'instance count of loveseat: ' . Loveseat::$loveCount;
