043395c93c553668050ba1018ed6bb9fb052e7fa
Seedling <david.r.graham32@gmail.com>
2024-09-23 10:27:26 -0400

    create public __construct method using an empty array and ternary shorthand to set properties

--- asgn05/bike-challenge/private/classes/bicycle.class.php
+++ asgn05/bike-challenge/private/classes/bicycle.class.php
@@ -12,7 +12,7 @@ class Bicycle
   public $gender;
   public $price;
   protected $weight_kg;
-  protected $condition_kg;
+  protected $condition_id;
 
   public const CATEGORIES = ['Road', 'Mountain', 'Hybrid', 'Cruiser', 'City', 'BMX'];
 
@@ -25,4 +25,18 @@ class Bicycle
     4 => 'Great',
     5 => 'Like new'
   ];
+
+  public function __construct($args = [])
+  {
+    $this->brand = $args['brand'] ?? '';
+    $this->model = $args['model'] ?? '';
+    $this->year = $args['year'] ?? '';
+    $this->category = $args['category'] ?? '';
+    $this->color = $args['color'] ?? '';
+    $this->description = $args['description'] ?? '';
+    $this->gender = $args['gender'] ?? '';
+    $this->price = $args['price'] ?? 0;
+    $this->weight_kg = $args['weight_kg'] ?? 0.0;
+    $this->condition_id = $args['condition_id'] ?? 3;
+  }
 }
