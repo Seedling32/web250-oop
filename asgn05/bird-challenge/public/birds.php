19bf7f695cda9b00f74ade5e9ab51100c5a204bf
Seedling <david.r.graham32@gmail.com>
2024-09-24 13:22:37 -0400

    Fill in birds.php with foreach loop and table data info.

--- asgn05/bird-challenge/public/birds.php
+++ asgn05/bird-challenge/public/birds.php
@@ -1,40 +1,52 @@
-<?php 
-  require_once('../private/initialize.php');
-  $page_title = 'Sightings';
-  include(SHARED_PATH . '/public_header.php');
+<?php
+require_once('../private/initialize.php');
+$page_title = 'Sightings';
+include(SHARED_PATH . '/public_header.php');
 ?>
 
 <h2>Bird inventory</h2>
 <p>This is a short list -- start your birding!</p>
 
-/* 
-  Create a table. The header should reflect the headings in the wnc-birds.csv class.
-  Use a table border of 1 to make the display easier to read.
-*/
-   
-<?php
+<!-- Create a table. The header should reflect the headings in the wnc-birds.csv class.
+Use a table border of 1 to make the display easier to read. -->
+
+<table id="birds">
+  <tr>
+    <th>Common name</th>
+    <th>Habitat</th>
+    <th>Food</th>
+    <th>Nest placement</th>
+    <th>Behavior</th>
+    <th>Conservation level</th>
+    <th>Backyard tips</th>
+  </tr>
+
+  <?php
 
   $parser = new ParseCSV(PRIVATE_PATH . '/wnc-birds.csv');
   $bird_array = $parser->parse();
 
-/*
-  Create a foreach array using $bird_array as $args
-  Create a new instance of $bird
-*/
+  // Create a foreach array using $bird_array as $args
+  // Create a new instance of $bird
 
-  
-  
-/*
-  Create a table row that lists out all of the bird
-  properties.
 
-*/
-  
-  ?>
-      
-      <?php } ?>
+  foreach ($bird_array as $args) {
+    $bird = new Bird($args); ?>
 
-    </table>
+    <!-- Create a table row that lists out all of the bird
+    properties. -->
 
+    <tr>
+      <td><?php echo h($bird->common_name); ?></td>
+      <td><?php echo h($bird->habitat); ?></td>
+      <td><?php echo h($bird->food); ?></td>
+      <td><?php echo h($bird->nest_placement); ?></td>
+      <td><?php echo h($bird->behavior); ?></td>
+      <td><?php echo h($bird->conservation_id); ?></td>
+      <td><?php echo h($bird->backyard_tips); ?></td>
+    </tr>
+  <?php } ?>
+
+</table>
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
